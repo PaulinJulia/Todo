451762975a9b2931a87a9a0185a5382f6f523105
julia.paulin <julia.paulin@chasacademy.se>
2024-01-31 22:18:17 +0100

    some styling and types

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,41 @@ const addTodo = document.querySelector(
 ) as HTMLButtonElement;
 const allTodos = document.querySelector(".all-todos") as HTMLUListElement;
 const todoInput = document.querySelector("#todo-input") as HTMLInputElement;
-const checkedBox = document.querySelector("#completed") as HTMLInputElement;
+const checkBox = document.querySelector("#checkbox") as HTMLDivElement;
 const todoText = document.querySelector("#todoText") as HTMLInputElement;
+
+const todoListArray: Todo[] = [];
+type Todo = {
+  id: number;
+  title: string;
+};
+const id: number = todoListArray.length;
 
 const addToList = (todoInput: HTMLInputElement): void => {
   const todoInputValue: string = todoInput.value;
   const todoList: HTMLLIElement = document.createElement("li");
   todoList.classList.add("todo-list");
   todoList.innerHTML = `
-            <input type="checkbox" name="completed" id="completed" />
-            <label for="completed" id="todoText" contenteditable="true">${todoInputValue}</label>
-            <button type="button" id="delete-todo-button">Delete</button>`;
+            <div id="checkbox"></div>
+            <div class="todoText" contenteditable="true">${todoInputValue}</div>
+            <div id="remove-todo-button"><box-icon name='trash-alt'></box-icon></div>`;
   allTodos.appendChild(todoList);
   todoInput.value = "";
+
+  const todoData: Todo = {
+    id: id,
+    title: todoInputValue,
+  };
+  todoListArray.push(todoData);
 };
 addTodo.addEventListener("click", () => addToList(todoInput));
 
-function checkBoxChange(todoInput: HTMLInputElement) {
-  console.log("Checkedbox changed:", checkedBox.checked);
-  if (checkedBox.checked) {
-    todoInput.textContent = "line-through";
-  } else {
-    todoText.style.textDecoration = "none";
-  }
+/* function checkBoxChange(checkBox: any): void {
+    if (checkBox.classList.contains("checkBoxIcon")) {
+      console.log("Klick");
+      checkBox.classList.remove("checkBoxIcon");
+    } else {
+      checkBox.classList.add("checkBoxIcon");
+    }
 }
-checkedBox.addEventListener("change", () => checkBoxChange(todoInput));
-
-
-
-
+checkBox.addEventListener("click", () => checkBoxChange(checkBox)); */
